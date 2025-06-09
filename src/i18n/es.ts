1a064f0c5f63b142815cacc7c7e221707bd46a66
edgar martinez <desarrollo_01@dxt.com.mx>
2025-06-09 09:03:10 -0600

    se crearon layout y se configuraron librerias

---
+++ src/i18n/es.ts
@@ -0,0 +1,8 @@
+// src/i18n/es.ts
+export default {
+  hero: {
+    title: '¡Hola! Soy Juan Pérez',
+    description: 'Desarrollador frontend enfocado en crear experiencias interactivas y accesibles.',
+    contact: 'Contáctame en juan@example.com',
+  },
+};
